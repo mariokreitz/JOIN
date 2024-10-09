7956f466bdcaf0608e0525ed284527e5ff749bd8
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-09 16:23:36 +0200

    refactor: rewrite navbar.template.js and update associated styling
    - Rewrote the navbar.template.js for improved structure and readability
    - Updated the styling to enhance the visual appearance and alignment of the navbar

--- scripts/templates/navbar.template.js
+++ scripts/templates/navbar.template.js
@@ -1,7 +1,19 @@
 function getNavbarTemplate() {
   return /*html*/ ` 
     <div class="navbar-logo-container">
-      <img class="join-navbar-logo" src="assets/img/join-logo.png" alt="Join Logo"/> 
+      <img class="navbar-logo" src="assets/img/join-logo.png" alt="Join Logo"/> 
+    </div>
+    <div class="navbar-link-container">
+      <ul>
+        <li><a class="navbar-link" href="#"><img src="./assets/img/icons/summary.png" alt="Summary Icon">Summary</a></li>
+        <li><a class="navbar-link" href="#"><img src="./assets/img/icons/add-task.png" alt="Add Task Icon">Add Task</a></li>
+        <li><a class="navbar-link" href="#"><img src="./assets/img/icons/board.png" alt="Board Icon">Board</a></li>
+        <li><a class="navbar-link" href="./contacts.html"><img src="./assets/img/icons/contacts.png" alt="Contacts Icon">Contacts</a></li>
+      </ul>
+      <ul>
+        <li><a class="navbar-link" href="privacyPolicy.html">Privacy Policy</a></li>
+        <li><a class="navbar-link" href="legalNotice.html">Legal notice</a></li>
+      </ul>
     </div>
     `;
 }
