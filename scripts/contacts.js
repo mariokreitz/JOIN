4a6272841a041262a18d789ad64d38f230c521bb
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-09 10:03:46 +0200

    feat: initialize contacts page with Firebase data fetching and rendering
    - Created init function for the contacts page that fetches data from Firebase via dataRequest.js
    - Configured API retrieval from firebaseConfig.js for secure access
    - Added render function that utilizes the contactTemplate component to display fetched data

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,4 +1,33 @@
+const API_URL = firebaseConfig.apiKey;
+let contacts;
+
 async function init() {
-  const response = await fetchData(firebaseConfig.apiKey + ".json");
-  console.log(response);
+  await getData(API_URL);
+  await renderContacts();
+}
+
+async function getData(url) {
+  const data = await fetchData(url + ".json");
+  contacts = data.contacts || [];
+}
+
+async function renderContacts() {
+  const contactList = document.getElementById("contactList");
+  if (!contactList) return;
+
+  console.log(contacts);
+
+  contactList.innerHTML = contacts
+    .map((contact) => {
+      const initials = getInitialsFromContact(contact);
+      return getContactTemplate(initials, contact.name, contact.email);
+    })
+    .join("");
+}
+
+function getInitialsFromContact({ name }) {
+  const [firstName, lastName] = name.split(" ");
+  const firstInitial = firstName.charAt(0);
+  const lastInitial = lastName.charAt(0);
+  return `${firstInitial}${lastInitial}`;
 }
