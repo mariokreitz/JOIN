4435e018b96c2a1a733ea6fdfc7f5e954aaa3a8d
Christian Zela <37801911+link947@users.noreply.github.com>
2024-10-09 16:23:16 +0200

    Added Inputfield Icons and other CSS

--- scripts/templates/editContactModal.js
+++ scripts/templates/editContactModal.js
@@ -7,22 +7,32 @@ function getEditContactModalTemplate() {
             <img src="./assets/svg/logo-white.svg" alt="Logo" />
           </div>
           <h2>Edit contact</h2>
+          <div class="underline"></div>
         </div>
         <div class="modal-right">
-          <button class="close-btn" onclick="closeEditContactModal()">&times;</button>
+          <button class="close-btn" onclick="closeEditContactModal()">
+            <img src="./assets/svg/close.svg" alt="" />
+          </button>
           <div class="modal-right-content">
             <div class="avatar">
               <span id="avatar-initials">TW</span>
             </div>
             <form class="contact-form">
-              <input type="text" id="contact-name" placeholder="Name" />
-              <input type="email" id="contact-email" placeholder="Email" />
-              <input type="tel" id="contact-phone" placeholder="Phone" />
+              <div class="input-container">
+                <input type="text" id="contact-name" placeholder="Name" />
+                <i class="icon-name"><img src="./assets/svg/person.svg" alt="" /></i>
+              </div>
+              <div class="input-container">
+                <input type="email" id="contact-email" placeholder="Email" />
+                <i class="icon-email"><img src="./assets/svg/mail.svg" alt="" /></i>
+              </div>
+              <div class="input-container">
+                <input type="tel" id="contact-phone" placeholder="Phone" />
+                <i class="icon-phone"><img src="./assets/svg/call.svg" alt="" /></i>
+              </div>
               <div class="form-actions">
                 <button class="delete-btn">Delete</button>
-                <button class="save-btn">
-                  Save <img class="check-mark" src="assets/svg/check-mark.svg" alt="Check Mark" />
-                </button>
+                <button class="save-btn">Save</button>
               </div>
             </form>
           </div>
