e21e656ae8dbe37914c1195c973120ce394262ba
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-09 15:28:37 +0200

    feat: update init() to load header and navbar components
    - Modified init() to now load the header and navbar components for improved modularity
    - Added JSDocs to document the changes and provide better clarity

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -2,12 +2,49 @@ const API_URL = firebaseConfig.apiKey;
 let contacts;
 
 /**
- * Initializes the app by fetching data and rendering the contacts.
- * @returns {Promise<void>} - A promise that resolves when the data has been fetched and the contacts have been rendered.
+ * Initializes the app by loading all necessary components and fetching data
+ * from the Realtime Database.
+ *
+ * @returns {Promise<void>}
  */
 async function init() {
+  loadComponents();
   await getData(API_URL);
   await renderContactList();
+}
+
+/**
+ * Loads all necessary components into the page.
+ *
+ * Currently, this function only loads the header and navbar components.
+ * @returns {void}
+ */
+function loadComponents() {
+  loadHeader();
+  loadNavbar();
+}
+
+/**
+ * Loads the header component into the element with the id "header".
+ * If no element with that id exists, this function does nothing.
+ * @returns {void}
+ */
+function loadHeader() {
+  const header = document.getElementById("header");
+  if (!header) return;
+
+  header.innerHTML = getHeaderTemplate();
+}
+
+/**
+ * Loads the navbar component into the element with the id "navbar".
+ * If no element with that id exists, this function does nothing.
+ * @returns {void}
+ */
+function loadNavbar() {
+  const navbar = document.getElementById("navbar");
+  if (!navbar) return;
+  navbar.innerHTML = getNavbarTemplate();
 }
 
 /**
