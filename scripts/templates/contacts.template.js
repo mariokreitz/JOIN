28d4efcd1301cacf69427a19a9e078b02b56acf1
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-09 10:57:54 +0200

    refactor: replace div tag with li tag for improved semantic structure
    - Changed div tag to li tag for better semantic HTML and list structure

--- scripts/templates/contacts.template.js
+++ scripts/templates/contacts.template.js
@@ -1,11 +1,11 @@
 function getContactTemplate(initials, fullName, email) {
   return /*html*/ `
-        <div class="contact-item">
+        <li class="contact-item">
             <div class="contact-initials">${initials}</div>
             <div>
                 <p class="contact-name">${fullName}</p>
                 <p class="contact-email">${email}</p>
             </div>
-        </div>
+        </li>
     `;
 }
