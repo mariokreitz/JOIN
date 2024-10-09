fa0a58e5465405882f9acc60c92dca409c36643f
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-09 15:27:37 +0200

    Create header.template.js

---
+++ scripts/templates/header.template.js
@@ -0,0 +1,11 @@
+function getHeaderTemplate() {
+  return /*html*/ `
+        <h1 class="inter-extralight heading-text">Kanban Project Management Tool</h1>
+        <div class="header-right">
+            <img class="qeustion-mark" src="./assets/img/icons/qeustion-mark.png">
+            <div class="profile-icon">
+                <span>G</span>
+            </div>
+        </div>
+    `;
+}
