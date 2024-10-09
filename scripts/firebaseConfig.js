319ac6c0dd5602361ece09c8a2046a3c0a9173d7
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-09 09:21:32 +0200

    Create firebaseConfig.js

---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef {Object} FirebaseConfig
+ * @property {string} apiKey - The Firebase Realtime Database API key.
+ */
+
+/**
+ * The Firebase Realtime Database configuration.
+ * @type {FirebaseConfig}
+ */
+const firebaseConfig = {
+  /**
+   * The Firebase Realtime Database API key.
+   * @type {string}
+   */
+  apiKey: "https://join-1e7a5-default-rtdb.europe-west1.firebasedatabase.app/",
+};
