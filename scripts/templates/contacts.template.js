fd258f29acd8655494f84e19269e5c78edadc125
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-09 12:20:35 +0200

    docs: add JSDocs to getContactTemplate()
    - Added JSDocs to the getContactTemplate() function for better documentation and clarity

--- scripts/templates/contacts.template.js
+++ scripts/templates/contacts.template.js
@@ -1,3 +1,11 @@
+/**
+ * Returns an HTML template for a contact item.
+ *
+ * @param {string} initials - The initials of the contact.
+ * @param {string} fullName - The full name of the contact.
+ * @param {string} email - The email address of the contact.
+ * @returns {string} The HTML template for the contact item.
+ */
 function getContactTemplate(initials, fullName, email) {
   return /*html*/ `
         <li class="contact-item">
