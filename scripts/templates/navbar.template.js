97ec8ba4a56f4a0276c42149535935195bb4e922
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-09 15:27:39 +0200

    Create navbar.template.js

---
+++ scripts/templates/navbar.template.js
@@ -0,0 +1,7 @@
+function getNavbarTemplate() {
+  return /*html*/ ` 
+    <div class="navbar-logo-container">
+      <img class="join-navbar-logo" src="assets/img/join-logo.png" alt="Join Logo"/> 
+    </div>
+    `;
+}
