c50531a5f962f8373e9b180270a3be7187d87db3
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-10-12 20:11:52 +0200

    add help.html to qeustion mark icon

--- scripts/templates/header.template.js
+++ scripts/templates/header.template.js
@@ -2,7 +2,7 @@ function getHeaderTemplate() {
   return /*html*/ `
         <h2 class="inter-extralight heading-text">Kanban Project Management Tool</h2>
         <div class="header-right">
-            <img class="qeustion-mark" src="./assets/img/icons/qeustion-mark.png">
+            <a href="help.html"><img class="qeustion-mark" src="./assets/img/icons/qeustion-mark.png" /></a>
             <div class="profile-icon">
                 <span>G</span>
             </div>
