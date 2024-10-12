92e4502a4cff8325a6f8ee9f111629cbd893eb7c
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-10-12 15:40:40 +0200

    change help.html

---
+++ scripts/legal-notice.js
@@ -0,0 +1,38 @@
+function init() {
+  loadComponents();
+}
+
+/**
+ * Loads all necessary components into the page.
+ *
+ * Currently, this function only loads the header and navbar components.
+ * @returns {void}
+ */
+function loadComponents() {
+  loadHeader();
+  loadNavbar();
+}
+
+/**
+ * Loads the header component into the element with the id "header".
+ * If no element with that id exists, this function does nothing.
+ * @returns {void}
+ */
+function loadHeader() {
+  const header = document.getElementById("header");
+  if (!header) return;
+
+  header.innerHTML = getHeaderTemplate();
+}
+
+/**
+ * Loads the navbar component into the element with the id "navbar".
+ * If no element with that id exists, this function does nothing.
+ *
+ * @returns {void}
+ */
+function loadNavbar() {
+  const navbar = document.getElementById("navbar");
+  if (!navbar) return;
+  navbar.innerHTML = getNavbarTemplate("legal");
+}
