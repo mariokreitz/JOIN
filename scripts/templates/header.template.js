79364a6c367d62aa3419c17396c6ff5c894253f6
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-14 15:12:28 +0200

    style: add font styling to span element in header profile icon
    - Applied font styling to the span element nested within the profile icon in the header for improved appearance

--- scripts/templates/header.template.js
+++ scripts/templates/header.template.js
@@ -1,10 +1,11 @@
 function getHeaderTemplate() {
   return /*html*/ `
         <h2 class="inter-extralight heading-text">Kanban Project Management Tool</h2>
+        <img class="header-respo-logo" src="./assets/img/logo.png" alt="Join Logo">
         <div class="header-right">
             <a href="help.html"><img class="qeustion-mark" src="./assets/img/icons/qeustion-mark.png" /></a>
             <div class="profile-icon">
-                <span>G</span>
+                <span class="inter-medium">G</span>
             </div>
         </div>
     `;
