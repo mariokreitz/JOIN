fe7edad4637834f5699e10f775738c6341b3df2b
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-15 11:59:53 +0200

    feat: updated toastMessage template for variable content
    - Modified the toastMessage template to accept a text parameter.
    - Enabled dynamic content display for improved notifications.
    - Added JSDoc comments for better documentation and usability.
    - Ensured compatibility with existing message types and styles.

--- scripts/templates/toastMessage.template.js
+++ scripts/templates/toastMessage.template.js
@@ -1,7 +1,13 @@
-function getToastMessage() {
+/**
+ * Returns an HTML string representing a toast message box, given a text.
+ *
+ * @param {string} text - The text to be displayed in the toast message box.
+ * @returns {string} An HTML string representing the toast message box.
+ */
+function getToastMessage(text) {
   return /*html*/ `
     <div id="toast-message" class="toast-message-box">
-      <p class="toast-message-text inter-extralight">Contact succesfully created</p>
+      <p class="toast-message-text inter-extralight">${text}</p>
     </div>
     `;
 }
