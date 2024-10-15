a8fdd785f28387946fc875042d24227823835d23
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-15 13:41:31 +0200

    feat: created board.html and script
    - Added board.html for future task board functionality.
    - Created the associated script file for managing board-related logic.

---
+++ scripts/board.js
@@ -0,0 +1,45 @@
+/**
+ * Initializes the page by loading the necessary components and rendering
+ * the contact list.
+ *
+ * @returns {Promise<void>} A promise that resolves when the page has been
+ * initialized.
+ */
+async function init() {
+  loadComponents();
+}
+
+/**
+ * Loads all necessary components into the page.
+ *
+ * Currently, this function only loads the header and navbar components.
+ * @returns {void}
+ */
+function loadComponents() {
+  loadHeader();
+  loadNavbar();
+}
+
+/**
+ * Loads the header component into the element with the id "header".
+ * If no element with that id exists, this function does nothing.
+ * @returns {void}
+ */
+function loadHeader() {
+  const header = document.getElementById("header");
+  if (!header) return;
+
+  header.innerHTML = getHeaderTemplate();
+}
+
+/**
+ * Loads the navbar component into the element with the id "navbar".
+ * If no element with that id exists, this function does nothing.
+ *
+ * @returns {void}
+ */
+function loadNavbar() {
+  const navbar = document.getElementById("navbar");
+  if (!navbar) return;
+  navbar.innerHTML = getNavbarTemplate("board");
+}
