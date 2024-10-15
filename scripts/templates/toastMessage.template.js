660aaebf46e695065592cfbd3688cc27f14e0717
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-15 11:56:26 +0200

    feat: created toastMessage template in JavaScript
    - Developed a reusable toast message component for notifications.
    - Ensured responsive design for different screen sizes.

---
+++ scripts/templates/toastMessage.template.js
@@ -0,0 +1,7 @@
+function getToastMessage() {
+  return /*html*/ `
+    <div id="toast-message" class="toast-message-box">
+      <p class="toast-message-text inter-extralight">Contact succesfully created</p>
+    </div>
+    `;
+}
