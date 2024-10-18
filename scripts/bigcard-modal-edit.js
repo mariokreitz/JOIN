8b03af0edbc4d1cf152467e62285ebb9b2a19cf7
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-10-18 15:46:20 +0200

    add prio selct color for all 3 prios

--- scripts/bigcard-modal-edit.js
+++ scripts/bigcard-modal-edit.js
@@ -1,4 +1,14 @@
 function toggleUrgentState() {
-  var container = document.getElementById("bc-select-urgent");
+  const container = document.getElementById("bc-select-urgent");
+  container.classList.toggle("active");
+}
+
+function toggleMediumState() {
+  const container = document.getElementById("bc-select-medium");
+  container.classList.toggle("active");
+}
+
+function toggleLowState() {
+  const container = document.getElementById("bc-select-low");
   container.classList.toggle("active");
 }
