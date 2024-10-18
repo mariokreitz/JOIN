1a5c2d05274d19419276617947593982d3d7a545
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-10-18 16:05:53 +0200

    add in function in addSubtask
    check if nothing in the render container

--- scripts/bigcard-modal-edit.js
+++ scripts/bigcard-modal-edit.js
@@ -26,7 +26,13 @@ function resetAllStates() {
 function addSubtaskBC() {
   const inputRef = document.getElementById("input-subtask-bc");
   const subtaskRenderContainer = document.getElementById("show-subtask-bc");
-  const subtask = inputRef.value;
+  const subtask = inputRef.value.trim();
+
+  if (subtask) {
+    if (subtaskRenderContainer.innerHTML === "") {
+      subtaskRenderContainer.innerHTML = "<ul></ul>";
+    }
+  }
 
   subtaskRenderContainer.innerHTML += <li>${subtask}</li>;
   subtask = "";
