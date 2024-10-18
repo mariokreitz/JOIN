0c3627fdc997a1d93ee59d92b58d5d58ef9c054c
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-10-18 19:17:31 +0200

    ADD Assigned Dropdown menü

--- scripts/bigcard-modal-edit.js
+++ scripts/bigcard-modal-edit.js
@@ -116,3 +116,21 @@ function getTemplate() {
       </button>
     </div>`;
 }
+
+function toggleAssignedDropdown() {
+  const dropdownContent = document.getElementById("dropdown-content");
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+}
+
+function closeDropdown(event) {
+  if (!event.target.matches(".dropdown")) {
+    const dropdownContent = document.getElementById("dropdown-content");
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    }
+  }
+}
