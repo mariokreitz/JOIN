7cd0a88564cbfe35e0c4b3963497b0843f4d2dba
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-10-18 15:48:41 +0200

    add resetall function to toggle state function

--- scripts/bigcard-modal-edit.js
+++ scripts/bigcard-modal-edit.js
@@ -1,14 +1,17 @@
 function toggleUrgentState() {
+  resetAllStates();
   const container = document.getElementById("bc-select-urgent");
   container.classList.toggle("active");
 }
 
 function toggleMediumState() {
+  resetAllStates();
   const container = document.getElementById("bc-select-medium");
   container.classList.toggle("active");
 }
 
 function toggleLowState() {
+  resetAllStates();
   const container = document.getElementById("bc-select-low");
   container.classList.toggle("active");
 }
