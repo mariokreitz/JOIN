0ac07688328148edb0759d00fa0a6011a12e6d40
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-10-26 21:44:29 +0200

    add change pw icon on input

--- scripts/login.js
+++ scripts/login.js
@@ -8,16 +8,27 @@ function validateForm() {
 
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailPattern.test(email)) {
-    document.getElementById("emailError").textContent = "Bitte geben Sie eine gültige E-Mail-Adresse ein.";
+    document.getElementById("emailError").textContent = "Please enter a valid email address.";
     isValid = false;
   }
 
   if (password.length < 7) {
-    document.getElementById("passwordError").textContent = "Das Passwort muss mindestens 7 Zeichen lang sein.";
+    document.getElementById("passwordError").textContent = "The password must be at least 7 characters long.";
     isValid = false;
   }
+}
 
-  if (isValid) {
-    alert("Anmeldung erfolgreich!");
+function toggleLoginIconVisibility() {
+  const passwordInput = document.getElementById("password");
+  const loginIconDiv = document.getElementById("login-change-pw-icon");
+
+  if (passwordInput.value.trim() !== "") {
+    loginIconDiv.classList.remove("login-svg");
+    loginIconDiv.classList.add("login-pw-non-visibility");
+  } else {
+    loginIconDiv.classList.remove("login-pw-non-visibility");
+    loginIconDiv.classList.add("login-svg");
   }
 }
+
+document.getElementById("password").addEventListener("input", toggleLoginIconVisibility);
