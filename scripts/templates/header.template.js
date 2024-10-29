e584b60e000cac516eaef42728d4ff8275238302
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-29 17:17:16 +0100

    style: rename CSS class and update element type
    - Renamed `.profile-icon` class to `.btn-profile`.
    - Changed the `<a>` tag to a `<button>` tag for better semantics.
    - Updated styles to ensure the button matches the overall design.

--- scripts/templates/header.template.js
+++ scripts/templates/header.template.js
@@ -10,11 +10,20 @@ function getHeaderTemplate() {
         <div class="header-right">
             ${
               currentUser.isLoggedIn
-                ? `<a href="./help.html"><img class="qeustion-mark" src="./assets/img/icons/qeustion-mark.png" /></a><div class="profile-icon"><span class="inter-medium">${getInitialsFromContact(
-                    currentUser
-                  ).toUpperCase()}</span></div>`
+                ? /*html*/ `
+                <a href="./help.html"><img class="qeustion-mark" src="./assets/img/icons/qeustion-mark.png" /></a>
+                <button onclick="openProfileMenu()" type="button" class="btn btn-profile">
+                  <span class="inter-medium">${getInitialsFromContact(currentUser).toUpperCase()}</span>
+                </button>
+                `
                 : ""
             }
+          <div id="profile-menu" class="profile-menu d_none">
+            <button onclick="" class="inter-extralight">Help</button>
+            <button onclick="" class="inter-extralight">Legal Notice</button>
+            <button onclick="" class="inter-extralight">Privacy Policy</button>
+            <button onclick="logout()" class="inter-extralight">Log Out</button>
+          </div>
         </div>
     `;
 }
