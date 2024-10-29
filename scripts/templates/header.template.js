ea02e2c8fbb64294445f1905afd93295c738a4ef
Mario Kreitz <Mario.Kreitz@web.de>
2024-10-29 13:59:45 +0100

    feat: conditional rendering of header profile and help sections
    - Render profile and help sections only if user is logged in.

--- scripts/templates/header.template.js
+++ scripts/templates/header.template.js
@@ -8,10 +8,13 @@ function getHeaderTemplate() {
         <h2 class="inter-extralight heading-text">Kanban Project Management Tool</h2>
         <img class="header-respo-logo" src="./assets/img/logo.png" alt="Join Logo">
         <div class="header-right">
-            <a href="help.html"><img class="qeustion-mark" src="./assets/img/icons/qeustion-mark.png" /></a>
-            <div class="profile-icon">
-                <span class="inter-medium">G</span>
-            </div>
+            ${
+              currentUser.isLoggedIn
+                ? `<a href="./help.html"><img class="qeustion-mark" src="./assets/img/icons/qeustion-mark.png" /></a><div class="profile-icon"><span class="inter-medium">${getInitialsFromContact(
+                    currentUser
+                  ).toUpperCase()}</span></div>`
+                : ""
+            }
         </div>
     `;
 }
