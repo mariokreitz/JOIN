d22529208e2b96a0807ed8bafff19509b4fc135a
Christian Zela <37801911+link947@users.noreply.github.com>
2024-10-29 18:15:02 +0200

    Added logo animation + adjusted login screen css

--- scripts/login.js
+++ scripts/login.js
@@ -80,6 +80,35 @@ function fillDemoCredentialsInLoginForm({ email, password }) {
   passwordInputField.value = password;
 }
 
+function setLogoFinalPosition() {
+  const logo = document.getElementById("login-logo");
+  logo.style.width = "90px";
+  logo.style.height = "110px";
+  logo.style.left = "60px";
+  logo.style.top = "55px";
+  document.getElementById("overlay").style.display = "none";
+  document.body.style.overflow = "auto";
+}
+
+function triggerLogoAnimation() {
+  const overlay = document.getElementById("overlay");
+  if (document.referrer.includes("signup.html")) {
+    setLogoFinalPosition();
+    return;
+  }
+  document.body.style.overflow = "hidden";
+  setTimeout(() => {
+    overlay.classList.add("fade-out");
+    const logo = document.getElementById("login-logo");
+    logo.classList.add("logo-animate");
+
+    setTimeout(() => {
+      overlay.style.display = "none";
+      document.body.style.overflow = "auto";
+    }, 1000);
+  }, 2000);
+}
+
 function validateLoginFormData() {
   const emailErrorElement = document.getElementById("emailError");
   const passwordErrorElement = document.getElementById("passwordError");
